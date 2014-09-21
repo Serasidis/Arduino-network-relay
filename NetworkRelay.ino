e644afebf3fbc4db9fae564e23be8107acb8804c
Vassilis Serasidis <avrsite@yahoo.gr>
2014-09-21 20:08:00 +0300

    Create NetworkRelay.ino

---
+++ NetworkRelay.ino
@@ -0,0 +1,323 @@
+/*
+
+ Network controlled relay (based on ENC28J60 ethernet module)
+ 
+ 
+
+ created:    31 Aug 2014 by vassilis Serasidis
+  Author:    Vassilis Serasidis
+    Home:    http://www.serasidis.gr
+   email:    avrsite@yahoo.gr, info@serasidis.gr
+ Version:    0.2
+ Updated:    06 Sept 2014
+ 
+ -= Commands =-
+  1*           asks for humidity, temperature, relay status and switch status (0=OFF, 1=ON)
+  p1234r01*    password 1234, relay0 -> ON
+  p1234r00*    password 1234, relay0 -> OFF
+  p1234c5678*  change password from 1234 to 5678
+  s0*          Sends the switch status (0=released)
+  s1*          Sends the switch status (1=released)
+  q*           Sends the connection close command (connection timeout) to android software.
+  
+  If a logic level change is occurred on switch pin, the arduino sends its status trough network to basic 4 android (s0* or s1*).
+  
+ 
+  Data stream format: dhh.hh#tt.tt#rs*
+  d     - Initial character. It's always 'd' ('d' is for data)
+  hh.hh - Humidity value from DHT22 (%)
+  tt.tt - Temperature value from DHT22 (C)
+  r     - Relay status 1=ON, 0=OFF
+  s     - Switch status 1=Pressed, 0=Released
+  *     - Is the finalizing character. Used to inform the android software that the incoming data stream has been completed.
+  
+  Example:
+  If you send the command "1*" (without the quotes) the arduino will return:
+        d52.10#29.30#10* 
+  Humidity 55.10 % - Temperature 29.30 C - Relay is ON (1) - Switch is released (0).
+
+  -= Connections =-
+  Arduino pin:   Usage:
+       7         button for restoring the password to '1234'
+       8         Input switch. On that pin is connected the optocoupler K814P.
+       9         Is connected the 12V DC relay through BC547 transistor. Relay is for driving electrical devices (motors, water pumps, adsl routers etc).
+  
+  This software is distributed under the GNU General Public License v3.0 http://www.gnu.org/copyleft/gpl.html
+  
+ */
+
+#define TIMEOUT1 20000 //5 seconds timout
+#define RELAY  9
+#define SWITCH 8
+#define DEFAULT_SETTINGS  7
+#define BUFFER_LENGTH 30
+
+
+#include <SPI.h>
+//#include <Ethernet.h>  //Select this line for using the stock ethernet library (W5100 ethernet module),
+
+//UIPEthernet library was written by Norbert Truchsess (https://github.com/ntruchsess/arduino_uip/archive/UIPEthernet_v1.57.zip)
+#include <UIPEthernet.h> //or select this line for using UIPEthernet library (ENC28J60 ethernet module).
+#include "DHT.h"  //Library written by Mark Ruys  (https://github.com/markruys/arduino-DHT)
+#include <EEPROM.h>
+
+unsigned long timer1 = 0;
+unsigned long timer2 = 0;
+unsigned long timer3 = 0;
+unsigned long timer4 = 0;
+
+boolean alreadyConnected = false; // whether or not the client was connected previously
+boolean connectionTIMEOUT1 = false;
+boolean relayStatus = false;
+boolean switchStatus = false;
+boolean switchStatusBackup = false;
+
+boolean stringComplete = false;
+String password;
+String serialData = "";
+unsigned char byteCounter;
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xEE, 0xED
+};
+IPAddress ip(192, 168, 1, 240);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 23 is default for telnet;
+EthernetServer server(10000);
+DHT dht;
+
+
+void setup() {
+  int i;
+  unsigned char eepromData;
+  pinMode(RELAY, OUTPUT);
+  pinMode(SWITCH, INPUT);
+  pinMode(DEFAULT_SETTINGS, INPUT);
+  digitalWrite(DEFAULT_SETTINGS, HIGH); //Enabler internal pull-up resistor.
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  
+  dht.setup(2); // data pin 2
+  byteCounter = 0;
+  
+  // start the Ethernet connection:
+  Ethernet.begin(mac, ip);
+
+  // start listening for clients
+  server.begin();
+  
+  if (EEPROM.read(0) == 0xff)  //If EEPROM is empty, write the default ASCII password '1234' in eeprom.
+  {
+    for(i=0;i<4;i++)
+      EEPROM.write(i,0x31 + i);
+    EEPROM.write(4,0); 
+  }
+  
+  i=0;
+  password = "";
+  //Read the password from EEPROM and store it to 'password' string.
+  for(i=0;i<4;i++)
+  {
+    char passwd = EEPROM.read(i);
+    password += passwd;
+  }
+  //Serial.print(password); //Show on serial port the password.
+  
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+}
+
+void loop()
+{
+    checkForClient();
+    timer4 = millis();
+    
+    while(digitalRead(DEFAULT_SETTINGS) == LOW) // Restore factory default password '1234'.
+    {
+      if(millis() > timer4 + 3000)
+      {
+        Serial.println("Restore the default password '1234'. Change it as soon as possible");
+        EEPROM.write(0,'1');
+        EEPROM.write(1,'2');
+        EEPROM.write(2,'3');
+        EEPROM.write(3,'4');
+        EEPROM.write(4,0); //Terminal character
+        password = "1234";
+        while(digitalRead(DEFAULT_SETTINGS) == LOW)
+          delay(200);
+      }      
+    }
+    
+}
+
+//----------------------------------------------------------------------
+//
+//----------------------------------------------------------------------
+void checkForClient()
+{
+  // Check for incoming clients
+  EthernetClient client = server.available();
+  
+  if (client)
+  {
+      timer1 = millis();
+      timer3 = millis();
+      while((client.connected()&&(millis() < (timer3 + TIMEOUT1))))
+      {
+        switchStatus = digitalRead(SWITCH);
+        if(switchStatus != switchStatusBackup)
+        {
+          server.write('b');
+          if(switchStatus == HIGH)
+            server.write('1');
+          else
+            server.write('0');
+          server.write('*');
+          switchStatusBackup = switchStatus;
+        }
+          
+        while((client.available()) > 0)
+        {
+           char inData = client.read();
+
+           if (inData != '*')
+           {
+             serialData += inData; 
+             byteCounter++;
+           }
+           else
+           {
+             serialData += '*';
+             checkIncomingData(byteCounter);
+             serialData = "";
+             byteCounter = 0;
+           }         
+            //Serial.write(inData);
+            timer3 = millis();
+        } 
+        
+        while(Serial.available() > 0)
+        {
+          char inData2 = (char)Serial.read();
+          server.write(inData2); 
+          timer3 = millis();
+        }
+      }
+      server.write('q'); //Send the disconnect command to android software.
+      server.write('*');
+      client.stop();
+  }          
+}
+
+//======================================================
+//
+//======================================================
+void getValuesFromSensor()
+{
+  digitalWrite(5, HIGH);
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature(); 
+  server.print("h"); 
+  server.println(humidity);
+  server.print("t"); 
+  server.println(temperature);
+  server.print("r");
+  if(relayStatus == true)
+    server.println("1");
+  else
+    server.println("0");
+  
+  digitalWrite(5, LOW);
+}
+
+//======================================================
+//
+//======================================================
+void checkIncomingData(unsigned char stringLength)
+{
+  int i;
+  char data = serialData[0];
+ 
+  //Serial.print(stringLength); //For debugging purpose only
+  
+  switch(data)
+  {
+    case '1':
+      server.write('d'); //d means for 'Data'
+      server.print(dht.getHumidity());
+      server.write('#');
+      server.print(dht.getTemperature());
+      server.write('#');
+      if(relayStatus == true)
+        server.write('1');
+      else
+        server.write('0');
+        
+      if(switchStatus == HIGH)
+        server.write('0');
+      else
+        server.write('1');
+        
+      server.write('*'); 
+    break;
+    
+    case 'p':
+      String newPassword = "";
+      for(i=1;i<stringLength;i++)
+      {
+        if((serialData[i] >= '0')&&(serialData[i] <= '9')) //Password string must have only numbers 0-9
+          newPassword += serialData[i];
+        else
+          break;       
+      }
+      if(newPassword.equals(password))
+      {
+        if((serialData[i] == '*'))
+        {
+          server.write('s');
+          server.write('1');
+          server.write('*');
+        }
+        
+        if((serialData[i] == 'r')&& (serialData[i+2] == '1'))
+        {
+          digitalWrite(RELAY, HIGH);
+          relayStatus = true;
+        }
+        
+        if((serialData[i] == 'r')&& (serialData[i+2] == '0'))
+        {
+          digitalWrite(RELAY, LOW);
+          relayStatus = false;
+        }
+          
+        if(serialData[i] == 'c') //Change password command
+        {
+          newPassword = "";
+          i++;
+          for(;i<stringLength;i++)
+          {
+            if((serialData[i] >= '0')&&(serialData[i] <= '9'))
+              newPassword += serialData[i];
+            else
+              break; //Terminate 'for' loop
+          }
+          password = newPassword; //Change password to the new one.
+          for (i=0;i<password.length();i++)  //Read the password from EEPROM and store it to variable 'pin'
+            EEPROM.write(i,password[i]);
+        }
+      }
+      else
+      {
+        server.write('s');
+        server.print("0");
+        server.print('*');
+      }
+    break;
+  }
+}
